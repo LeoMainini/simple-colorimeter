61f0cc96f2231166e088509940d158b99207808d
leferrei <leferrei@student.42lisboa.com>
2023-06-16 02:34:27 +0100

    add padding to rgb values - add safety on non img files

--- main.py
+++ main.py
@@ -11,26 +11,31 @@ Bootstrap(app)
 
 
 class FileForm(FlaskForm):
-    file = FileField('File', validators=[DataRequired()])
-    submit = SubmitField('Upload')
+	file = FileField('File', validators=[DataRequired()])
+	submit = SubmitField('Upload')
 
 
 def extract_pallete(img):
-    colors = colorgram.extract(img, 9)
-    return colors
+	try:
+		colors = colorgram.extract(img, 9)
+	except:
+		colors = 0
+	return colors
 
 
 @app.route("/", methods=["GET","POST"])
 def home():
-    form = FileForm()
-    
-    if form.validate_on_submit():
-        print(form.data)
-        color_data = extract_pallete(form.file.data)
-        color_list=[color.rgb for color in color_data]
-        pct_list = [f"{round(color.proportion*100,2)}%" for color in color_data]
-        return render_template("index.html", form=form, colors=color_list, pct = pct_list)
-    return render_template("index.html", form=form)
+	form = FileForm()
+	
+	if form.validate_on_submit():
+		print(form.data)
+		color_data = extract_pallete(form.file.data)
+		if (color_data == 0):
+			return render_template("index.html", form=form)
+		color_list=[color.rgb for color in color_data]
+		pct_list = [f"{round(color.proportion*100,2)}%" for color in color_data]
+		return render_template("index.html", form=form, colors=color_list, pct = pct_list)
+	return render_template("index.html", form=form)
 
 
 
@@ -40,4 +45,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
